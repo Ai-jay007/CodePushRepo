58c2526aa7ef4a9a02862630a2f648095fc8a1ec
Ajay vaishnav <53658682+Ai-jay007@users.noreply.github.com>
2026-02-19 16:08:39 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,7 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("welcome to devops day-2 session");
-				System.out.println("welcome to devops day-3 session");
-				System.out.println("welcome to devops day-4 session");
+				
 	}
 }
 
