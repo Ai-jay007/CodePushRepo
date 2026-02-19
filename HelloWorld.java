92d2742afe043238d1bff1eaec70a3a085b6e0fb
Ajay vaishnav <53658682+Ai-jay007@users.noreply.github.com>
2026-02-19 16:08:58 +0530

    Add welcome messages for devops day-3 and day-4

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("welcome to devops day-2 session");
+		System.out.println("welcome to devops day-3 session");
+				System.out.println("welcome to devops day-4 session");
 	}
 }
 
